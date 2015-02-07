50084a3dd96fda550d839e7cc206c6e7a95f2dce
J Sylka <sylkajm@gmail.com>
2015-02-07 15:32:32 -0500

    Changed html
    changed html inside so that it will scroll to the contact us section

--- sections/contact.php
+++ sections/contact.php
@@ -7,11 +7,15 @@
  */
 
 ?>
-<div class="row">
-	<div class="col-md-6">
-
-	</div>
-	<div class="col-md-6">
-
-	</div>
-</div>
+ <!-- Contact Section -->
+<section id="contact" class="contact-section">
+   <div class="container">
+       <div class="row">
+           <div class="col-lg-6">
+               <h1>Contact Section</h1>
+           </div>
+           <div class="col-lg-6">
+           </div>
+       </div>
+   </div>
+</section>
