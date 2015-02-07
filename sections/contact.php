8ce2e059f0d4e31955afe4c37dd2c8148fcf7890
J Sylka <sylkajm@gmail.com>
2015-02-07 15:35:14 -0500

    added new row
    added new row to contact page

--- sections/contact.php
+++ sections/contact.php
@@ -11,10 +11,16 @@
 <section id="contact" class="contact-section">
    <div class="container">
        <div class="row">
+           <div class="col-lg-12">
+               <h1>Contact Us</h1>
+           </div>
+       </div>
+       <div class="row">
            <div class="col-lg-6">
-               <h1>Contact Section</h1>
+
            </div>
            <div class="col-lg-6">
+
            </div>
        </div>
    </div>
