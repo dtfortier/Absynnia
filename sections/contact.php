54e0a2af7d50385e4f7f0e68f0617ba51133f491
danielle fortier <dtfortier@gmail.com>
2015-02-07 19:57:29 -0500

    new

--- sections/contact.php
+++ sections/contact.php
@@ -9,26 +9,26 @@
        <!-- contact information labels -->
        <div class="row">
            
-           <div class="col-lg-3">
+           <div class="col-lg-2">
               <div>
-             		 <h3>Address:</h3>
+             		 <p>Address:</p>
               </div>
               <!-- add non-breaking space to line up fields -->
               <div>&nbsp;</div>
               <div>
-                   <h3>Hours:</h3>                  
+                   <p>Hours:</p>                  
               </div>
               <!-- add non-breaking space to line up fields -->
               <div>&nbsp;</div>
               <div>
-                <h3>Phone:</h3>
+                <p>Phone:</p>
               </div>
               <div>
-                <h3>Email:</h3>
+                <p>Email:</p>
               </div>
             </div>
             <!-- contact information content -->
-            <div class="col-lg-3">
+            <div class="col-lg-2">
               
                 <div>
                     <p>25 Grant Street</p>
@@ -44,9 +44,10 @@
                 <div>
                     <p>zgemmeda@yahoo.com</p>
                 </div>
+            </div>
 
               <!-- ggogle maps link (small version) -->
-              <div class=col-lg-6">
+              <div class="col-lg-8">
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2922.0087709880618!2d-78.890042!3d42.914855999999986!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89d3131b41459539%3A0xf7b64402c84db7c6!2sWest+Side+Bazaar!5e0!3m2!1sen!2sus!4v1423343447601" width="400" height="300" frameborder="0" style="border:0"></iframe>           
               </div>
             
