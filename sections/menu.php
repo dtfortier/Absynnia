4aa2f929bdfbceef2c95c11495c9fb1ba8aa052d
jennifer sylka <sylkajm@gmail.com>
2015-02-07 19:35:17 -0500

    menu
    added menu

--- sections/menu.php
+++ sections/menu.php
@@ -3,7 +3,148 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
-                    <h1>Menu</h1>
+                    <h2>Menu</h2>
+                    <div class="wrap">
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <h1>Meat Entrees
+                                </h1>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <h2>#1. Beef Kay Wat ($9.99)</h2>
+                                <p>Chopped beef slowly simmered with berbere (hot spices) and a combination of seasonings
+                                </p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <h2>#2. Doro Wat  ($9.99)</h2>
+                                <p>Drumsticks marinated with onions, garlic, seasoned butter, berbere and a combination of seasonings
+                                </p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <h2>#3. Beef Alitcha ($9.99)</h2>
+                                <p>Chopped beef slowly simmered in a mild and flavorful herb sauce
+                                </p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <h2>#4. Kitfo ($12.00)</h2>
+                                <p>Finely chopped beef seasoned with herbal butter, mitmita, & seasoned in an Ethiopian herb mix (It can be ordered Rare, Medium or well done), and served with cheese and gomen
+                                </p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <h2>#5. Lamb Tibs (9.99)</h2>
+                                <p>Cubed tender lamb fried with onion, tomato, rosemary, peppers and flavorful herbs
+                                </p>
+                            </div>
+                        </div>
+                        
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <h2>#6 Beef Tips (9.99)</h2>
+                                <p>Cubed tender beef fried with onion, tomato, rosemary, peppers and flavorful herbs
+                                </p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <h2>#7. Gomen Besiga ($9.99)</h2>
+                                <p>Lean, cubed beef slow- cooked and blended with kale, peppers, gingero garlic and onions.
+                                </p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <h2>#8. Abyssinia</h2>
+                                <p>Vegetarian and meat entrees for a person ($9.99) for 2 person ($15.99)
+                                </p>
+                            </div>
+
+                            <!-- Vegetarian Entrees  -->
+                        
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <h1>Vegetarian Entrees
+                                </h1>
+                            </div>
+
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <h2>#9. Kay Sir & Dinich ($6.99)</h2>
+                                <p>Potatoes and fresh beets stewed and blended with garlic, ginger and onions
+                                </p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <h2>#10. Misir Wat ($6.99)</h2>
+                                <p>Red split lentils simmered in spicy berbere sauce
+                                </p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <h2>#11. Shiro Wat ($6.99)</h2>
+                                <p>Finely ground split peas, lentiis and chickpeas simmered in berbere and a combination of seasonings
+                                </p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <h2>#12. Kik ditcha ($6.99)</h2>
+                                <p>Yellow split peas simmered in a mild and flavorful herb sauce
+                                </p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <h2>#13. Quanta firfir ($8.99)</h2>
+                                <p>Dried strips of beef mixed with Injera with seasoned sauce
+                                </p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <h2>#14. Fosolia with Carrots ($6.99)</h2>
+                                <p>String beans lightly spiced and sautded with carrots and potatoes.
+                                </p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <h2>#15. Tikil Gomen ($6.99)</h2>
+                                <p>lightly spiced cabbage, carrots, onions and tomatoes stewed in a mild sauce
+                                </p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <h2>#16. Ye' Abesha Gomen ($6.99)</h2>
+                                <p>Kale, peppers, ginger, garlic and onions slow-cooked in a mild sauce
+                                </p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <h2>Vegetarian Combo: </h2>
+                                <p>All vegetarian entrees: 1 person ($8.99), for 2 people ($14.99)
+                                </p>
+                            </div>
+                        </div>
+
+
+                    </div>
                 </div>
             </div>
         </div>
