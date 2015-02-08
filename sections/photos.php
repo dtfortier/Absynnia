55b463c96047e019d7f730314a2ec693d450b816
jennifer sylka <sylkajm@gmail.com>
2015-02-07 19:50:32 -0500

    photo
    removed photo1

--- sections/photos.php
+++ sections/photos.php
@@ -8,12 +8,7 @@
             </div>
         
         <div class="row">
-            
-                <div class="col-md-4 thumb">
-                    <!-- <a class="thumbnail" href="#" data-image-id="" data-toggle="modal" data-title="This is my title" data-caption="Some lovely red flowers" data-image="http://onelive.us/wp-content/uploads/2014/08/flower-delivery-online.jpg" data-target="#image-gallery"> -->
-                        <img class="img-responsive" src="images/photo1.jpg" alt="Short alt text">
-                    <!--</a> -->
-                </div>
+
                 <div class="col-md-4 thumb">
                         <img class="img-responsive" src="images/photo2.jpg" alt="Short alt text">
                 </div>
