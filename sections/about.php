398117e60c53467f5d05060f4a44b436ec598d88
jennifer sylka <sylkajm@gmail.com>
2015-02-07 19:57:32 -0500

    hi
    hi

--- sections/about.php
+++ sections/about.php
@@ -19,6 +19,71 @@
                                     is eaten by scooping up toppings using the bread. The combinations of flavors is wonderful.</p>
                             </div>
                         </div>
+                        <div class="row">
+    <div class="col-md-12">
+<h2>TESTIMONIALS</h2>
+</div>
+</div>
+<div class="row">
+    <div class="col-md-12">
+<h3>5 stars*****<h3>
+<p> “A good friend recommended this place. He told me I could experience different kinds 
+
+of food, a lot of food without spending a lot. He was RIGHT. Very good food. I was disappointed that 
+
+it was closed today. Some must try items seafood noodles and beef curry from the Burmese window. 
+
+Spicy chicken from the Sudanese window. Beef tibs from the Ethiopian window. All very good.” </p>
+<p>
+
+-K. Quick; Facebook
+</p>
+</div>
+</div>
+<div class="row">
+    <div class="col-md-12">
+
+<h3>5 stars*****</h3><p> “Great food and great people my favorite is the Ethiopian place and a good place to get a 
+
+plate full of food for $5-6” </p>
+<p>
+-T. Lunge; Facebook</p>
+</div>
+</div>
+
+<div class="row">
+    <div class="col-md-12">
+<h3>
+
+5 stars***** </h3><p>“The food is amazing! The accessories on the retail side are cute and unique. The West 
+
+Side Bazaar a must try!!”</p><p>
+
+-A. Orazi; Facebook</p>
+</div>
+</div>
+<div class="row">
+    <div class="col-md-12">
+
+<p>You'll find some of the best brewed Ethiopian coffee here from Abyssinia” </p>
+
+<p>-Alex L.; Yelp.com </p>
+</div>
+</div>
+<div class="row">
+    <div class="col-md-12">
+
+<p>“Good coffee in fancy cups with friendly service. This is not something I was expecting but I was 
+
+very happy to have found in Buffalo. The lady was very genial and helpful. We ordered the traditional 
+
+coffee and a mixed fruit smoothie. The coffee was bold and smooth, and the smoothie was what the 
+
+daughter needed to chase away the tiredness of a long drive.“ </p><p>
+
+-Clarice O.;Yelp</p>
+</div>
+</div>
                     </div>
 
                 </div>
